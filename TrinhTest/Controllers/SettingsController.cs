68778e80cfe382ba27ad390b0d45f3bbe589843b
trinh <trinhlq@ominext.com>
2023-04-11 11:06:25 +0700

    TrinhLQ

---
+++ TrinhTest/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrinhTest.Controllers
+{
+    public class SettingsController : Controller
+    {
+        [Authorize]
+        [Route("PriceServices")]
+        public IActionResult PriceServices()
+        {
+            return View("~/Views/Pages/Setting/PriceServices/SettingPriceServices.cshtml");
+        }
+        [Authorize]
+        [Route("Languages")]
+        public IActionResult Languages()
+        {
+            return View("~/Views/Pages/Setting/Languages.cshtml");
+        }
+    }
+}
