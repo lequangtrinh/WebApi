aeee3637a77f340de642dd472e2c40a184ac3e4d
TrinhLQ <lequangtrinh0811@gmail.com>
2023-11-06 15:41:01 +0700

    .

---
+++ RabbitMQManageAPI/Consumer/TopicConsumer.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQManageAPI.Producer.Consumer
+{
+    public class TopicConsumer
+    {
+        private const string nameQueueSQL = "user-Sql-queue";
+        private const string nameQueueELK = "user-ELK-queue";
+        private const string nameExchange = "user-topic-exchange";
+        public static void Consume(IModel channel)
+        {
+            GetQueueSQL(channel);
+            GetQueueELK(channel);
+        }
+        public static void GetQueueSQL(IModel channel)
+        {
+            channel.ExchangeDeclare(nameExchange, ExchangeType.Topic, durable: true, autoDelete: false);
+
+            channel.QueueDeclare(nameQueueSQL,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            // *:phù hợp với bất kỳ tywf nào tương tự %like in sql
+            // #:khớp với 1 or nhiều từ bất kì
+            //channel.QueueBind(nameQueue, nameExchange, "sql.#");
+            channel.QueueBind(nameQueueSQL, nameExchange, "sql.#");
+            //channel.QueueBind("demo-topic-queue", "demo-topic-exchange", "*.update.*");
+            channel.BasicQos(0, 10, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) => {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+            };
+
+            channel.BasicConsume(nameQueueSQL, true, consumer);
+            Console.WriteLine("Consumer started SQL");
+        }
+        public static void GetQueueELK(IModel channel)
+        {
+            channel.ExchangeDeclare(nameExchange, ExchangeType.Topic, durable: true);
+
+            channel.QueueDeclare(nameQueueELK,
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+            // *:phù hợp với bất kỳ tywf nào tương tự %like in sql
+            // #:khớp với 1 or nhiều từ bất kì
+            channel.QueueBind(nameQueueELK, nameExchange, "*.elk");
+            channel.BasicQos(0, 10, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (sender, e) => {
+                var body = e.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+            };
+            channel.BasicConsume(nameQueueELK, true, consumer);
+            Console.WriteLine("Consumer started ELK");
+        }
+    }
+}
---
+++ RabbitMQManageAPI/TopicProducer.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace RabbitMQManageAPI.Producer
+{
+    static class TopicExchangePublisher
+    {
+        private const string nameExchange = "demo-topic-exchange";
+        public static void Publish<T>(IModel channel, T value)
+        {
+
+            var ttl = new Dictionary<string, object>
+            {
+                {"x-message-ttl", 30000 }
+            };
+            channel.ExchangeDeclare(nameExchange, ExchangeType.Topic, durable: true, autoDelete: false, arguments: ttl);
+            var message = new { Name = "data", Message = value };
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+            channel.BasicPublish(nameExchange, "sql.elk", null, body);
+
+        }
+    }
+}
